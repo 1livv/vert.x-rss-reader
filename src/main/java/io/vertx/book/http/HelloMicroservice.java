876eb489a93a4747d3b36072c78b36c840735ac6
Liviu Gheorghe <gheorghe@adobe.com>
2017-11-18 18:55:06 +0200

    initial commit

---
+++ src/main/java/io/vertx/book/http/HelloMicroservice.java
@@ -0,0 +1,12 @@
+package io.vertx.book.http;
+
+import io.vertx.core.AbstractVerticle;
+
+public class HelloMicroservice extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
